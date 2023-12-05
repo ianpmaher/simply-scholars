aad051137319bc76930e13381d4bdf01a762e07d
Ian Maher <ianpmaher@gmail.com>
2023-12-05 09:53:53 -0500

    header is up but need bulk of everything else

--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
 // import Image from "next/image";
 import DogBone from "./_components/DogBone";
 import JsonTest from "./_components/JsonTest";
+import Header from "./_components/shared/Header";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className="text-6xl font-bold text-center">Wow, this is a cool page!</h1>
-            <DogBone/>
-            <JsonTest/>
-        </main>
+        <div>
+            <Header/>
+            <main className="flex min-h-screen flex-col items-center justify-between p-24">
+                <h1 className="text-6xl font-bold text-center">Wow, this is a cool page!</h1>
+                <DogBone/>
+                <JsonTest/>
+            </main>
+        </div>
     );
 }
